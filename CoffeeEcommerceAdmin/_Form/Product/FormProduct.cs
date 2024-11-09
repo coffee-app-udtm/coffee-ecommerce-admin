d575f6f08939b240d7c9c704529e9e96e6e3979e
Nga Phan <phanthithanhnga1303@gmail.com>
2024-11-09 13:14:31 +0700

    refactor: change form to _form folder

--- CoffeeEcommerceAdmin/_Form/Product/FormProduct.cs
+++ CoffeeEcommerceAdmin/_Form/Product/FormProduct.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CoffeeEcommerceAdmin
+namespace CoffeeEcommerceAdmin._Form.Product
 {
     public partial class FormProduct : Form
     {
